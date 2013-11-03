420f634bb4e7c9111b23645035e0da181e5e50b5
jedmac <mac.jedrzejewski@gmail.com>
2013-11-03 13:48:03 +0100

    Change

---
+++ FancyTravellerMvc/FancyTravellerMvc/Models/Domain Layer/SampleServiceLayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FancyTravellerMvc.Models.Domain_Layer
+{
+    public class SampleServiceLayer
+    {
+    }
+}
---
+++ FancyTravellerMvc/FancyTravellerMvc/Models/ViewModels/SampleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FancyTravellerMvc.Models.ViewModels
+{
+    public class SampleViewModel
+    {
+    }
+}
