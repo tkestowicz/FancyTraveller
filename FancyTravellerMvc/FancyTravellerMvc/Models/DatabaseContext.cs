2ec1714cc5d547c1c508bffa1d26d05b024d568c
jedmac <mac.jedrzejewski@gmail.com>
2013-11-03 13:38:07 +0100

    Solution

---
+++ FancyTravellerMvc/FancyTravellerMvc/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FancyTravellerMvc.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        //DbSet<nazwa encji> encje { get; set; }
+    }
+}
