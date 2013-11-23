526a491d697af86f4acb1937296cc3ce81cee4e8
jedmac <mac.jedrzejewski@gmail.com>
2013-11-23 12:22:53 +0100

    Modified to get prototype for changes

--- Dijkstra/Dijkstra/DijkstraOperations.cs
+++ Dijkstra/Dijkstra/DijkstraOperations.cs
@@ -15,6 +15,44 @@ namespace Dijkstra
         private List<double> listOfDistances = new List<double>();
         private Queue<int> allVerticesQueue = new Queue<int>();
 
+        //public string DijkstraAlgorithm(int sourceTop, int allVertices, List<List<Tuple<int, int>>> listOfNeighboursDistance, int destinationTop)
+        //{
+        //    for (int i = 0; i < allVertices; ++i)
+        //    {
+        //        listOfDistances.Add(i);
+        //        listOfDistances[i] = double.PositiveInfinity;
+        //        allVerticesQueue.Enqueue(i);
+        //    }
+
+        //    listOfDistances[sourceTop] = 0;
+
+        //    do
+        //    {
+        //        verticePickedFromQueue = allVerticesQueue.Peek();
+
+        //        if (verticePickedFromQueue != 0)
+        //        {
+        //            for (int i = 0; i < listOfNeighboursDistance[verticePickedFromQueue].Count; ++i)
+        //            {
+
+        //                verticeNeighbour = listOfNeighboursDistance[verticePickedFromQueue][i].Item1;
+        //                toNeighbourCost = listOfNeighboursDistance[verticePickedFromQueue][i].Item2;
+
+        //                if (listOfDistances[verticeNeighbour] > listOfDistances[verticePickedFromQueue] + toNeighbourCost)
+        //                {
+        //                    listOfDistances[verticeNeighbour] = listOfDistances[verticePickedFromQueue] + toNeighbourCost;
+        //                    allVerticesQueue.Enqueue(verticeNeighbour);
+        //                }
+        //            }
+        //        }
+
+        //        allVerticesQueue.Dequeue();
+
+        //    } while (allVerticesQueue.Count > 0);
+
+        //    return "Shortest road from " + sourceTop + " to " + destinationTop + " is: " + listOfDistances[destinationTop].ToString() + " km";
+        //}
+
         public string DijkstraAlgorithm(int sourceTop, int allVertices, List<List<Tuple<int, int>>> listOfNeighboursDistance, int destinationTop)
         {
             for (int i = 0; i < allVertices; ++i)
@@ -52,5 +90,7 @@ namespace Dijkstra
 
             return "Shortest road from " + sourceTop + " to " + destinationTop + " is: " + listOfDistances[destinationTop].ToString() + " km";
         }
+
+        
     }
 }
