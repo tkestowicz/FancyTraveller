6dd3a6365ab16fd746b72781727a71f14db2e973
jedmac <mac.jedrzejewski@gmail.com>
2013-11-23 16:38:02 +0100

    FindShortestRoute changed to return int instead of string, parameter List<List<Tuple<int, int>>> changed to List<List<Vertex>> for this method

--- FancyTravellerApp/FancyTraveller.Domain/Logic/DijkstraRouteFinder.cs
+++ FancyTravellerApp/FancyTraveller.Domain/Logic/DijkstraRouteFinder.cs
@@ -13,8 +13,9 @@ namespace FancyTraveller.Domain.Logic
         private List<double> listOfDistances = new List<double>();
         private Queue<int> allVerticesQueue = new Queue<int>();
 
-        private string FindShortestPath(int sourceTop, int allVertices, List<List<Tuple<int, int>>> listOfNeighboursDistance, int destinationTop)
+        public int FindShortestRoute(int sourceTop, int destinationTop, int allVertices, List<List<Vertex>> vertices)
         {
+
             for (int i = 0; i < allVertices; ++i)
             {
                 listOfDistances.Add(i);
@@ -30,11 +31,11 @@ namespace FancyTraveller.Domain.Logic
 
                 if (verticePickedFromQueue != 0)
                 {
-                    for (int i = 0; i < listOfNeighboursDistance[verticePickedFromQueue].Count; ++i)
+                    for (int i = 0; i < vertices[verticePickedFromQueue].Count; ++i)
                     {
 
-                        verticeNeighbour = listOfNeighboursDistance[verticePickedFromQueue][i].Item1;
-                        toNeighbourCost = listOfNeighboursDistance[verticePickedFromQueue][i].Item2;
+                        verticeNeighbour = vertices[verticePickedFromQueue][i].DestinationCity.CityId;
+                        toNeighbourCost = vertices[verticePickedFromQueue][i].Distance;
 
                         if (listOfDistances[verticeNeighbour] > listOfDistances[verticePickedFromQueue] + toNeighbourCost)
                         {
@@ -48,13 +49,46 @@ namespace FancyTraveller.Domain.Logic
 
             } while (allVerticesQueue.Count > 0);
 
-            return "Shortest road from " + sourceTop + " to " + destinationTop + " is: " + listOfDistances[destinationTop].ToString() + " km";
+            return Convert.ToInt32(listOfDistances[destinationTop]);
         }
 
-        public int FindShortestRoute(string source, string destination, int allVertices, IEnumerable<Vertex> vertices)
-        {
-            //TODO: the method above should be rewritten.
-            return 0;
-        }
+        //private string FindShortestPath(int sourceTop, int allVertices, List<List<Tuple<int, int>>> listOfNeighboursDistance, int destinationTop)
+        //{
+        //for (int i = 0; i < allVertices; ++i)
+        //{
+        //    listOfDistances.Add(i);
+        //    listOfDistances[i] = double.PositiveInfinity;
+        //    allVerticesQueue.Enqueue(i);
+        //}
+
+        //listOfDistances[sourceTop] = 0;
+
+        //do
+        //{
+        //    verticePickedFromQueue = allVerticesQueue.Peek();
+
+        //    if (verticePickedFromQueue != 0)
+        //    {
+        //        for (int i = 0; i < listOfNeighboursDistance[verticePickedFromQueue].Count; ++i)
+        //        {
+
+        //            verticeNeighbour = listOfNeighboursDistance[verticePickedFromQueue][i].Item1;
+        //            toNeighbourCost = listOfNeighboursDistance[verticePickedFromQueue][i].Item2;
+
+        //            if (listOfDistances[verticeNeighbour] > listOfDistances[verticePickedFromQueue] + toNeighbourCost)
+        //            {
+        //                listOfDistances[verticeNeighbour] = listOfDistances[verticePickedFromQueue] + toNeighbourCost;
+        //                allVerticesQueue.Enqueue(verticeNeighbour);
+        //            }
+        //        }
+        //    }
+
+        //    allVerticesQueue.Dequeue();
+
+        //} while (allVerticesQueue.Count > 0);
+
+        //return "Shortest road from " + sourceTop + " to " + destinationTop + " is: " + listOfDistances[destinationTop].ToString() + " km";
+        //}
+
     }
 }
