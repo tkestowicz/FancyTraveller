9404a006c3af0404666ee88f55ac0e4b503ac4c3
jedmac <mac.jedrzejewski@gmail.com>
2013-12-29 10:28:37 +0100

    Unit test - get_shortest_route___with_no_cities_to_skip___shortest_path_is_returned() - arranged

--- FancyTravellerApp/FancyTraveller.Domain.Tests.Unit/Tests/RouteServiceTests.cs
+++ FancyTravellerApp/FancyTraveller.Domain.Tests.Unit/Tests/RouteServiceTests.cs
@@ -28,22 +28,43 @@ namespace FancyTraveller.Domain.Tests.Unit.Tests
         [Test]
         public void get_shortest_route___with_no_cities_to_skip___shortest_path_is_returned()
         {
-            const string source = "Amterdam";
-            const string destination = "Wroclaw";
-            var preparedData = new List<Vertex>(4)
+            //const string source = "Amterdam";
+            //const string destination = "Wroclaw";
+            const int source = 1;
+            const int destination = 2;
+            int numberOfCities = 4;
+            var preparedData = new List<Vertex>()
             {
-                new Vertex(){ SourceCity = new City(){ Name = "Amsterdam" }, DestinationCity = new City(){ Name = "Wroclaw"}, Distance = 1200},
-                new Vertex(){ SourceCity = new City(){ Name = "Amsterdam" }, DestinationCity = new City(){ Name = "Hannover"}, Distance = 450},
-                new Vertex(){ SourceCity = new City(){ Name = "Hannover" }, DestinationCity = new City(){ Name = "Wroclaw"}, Distance = 500},
-                new Vertex(){ SourceCity = new City(){ Name = "Amsterdam" }, DestinationCity = new City(){ Name = "Berlin"}, Distance = 900},
-                new Vertex(){ SourceCity = new City(){ Name = "Berlin" }, DestinationCity = new City(){ Name = "Wroclaw"}, Distance = 350}
+                new Vertex(){ SourceCity = new City(){ CityId = 1, Name = "Amsterdam" }, DestinationCity = new City(){ CityId = 2, Name = "Wroclaw"}, Distance = 1200},
+                new Vertex(){ SourceCity = new City(){ CityId = 2, Name = "Wroclaw" }, DestinationCity = new City(){ CityId = 1, Name = "Amsterdam"}, Distance = 1200},
+                new Vertex(){ SourceCity = new City(){ CityId = 1, Name = "Amsterdam" }, DestinationCity = new City(){ CityId = 3, Name = "Hannover"}, Distance = 450},
+                new Vertex(){ SourceCity = new City(){ CityId = 3, Name = "Hannover" }, DestinationCity = new City(){ CityId = 1, Name = "Amsterdam"}, Distance = 450},
+                new Vertex(){ SourceCity = new City(){ CityId = 3, Name = "Hannover" }, DestinationCity = new City(){ CityId = 2, Name = "Wroclaw"}, Distance = 500},
+                new Vertex(){ SourceCity = new City(){ CityId = 2, Name = "Wroclaw" }, DestinationCity = new City(){ CityId = 3, Name = "Hannover"}, Distance = 500},
+                new Vertex(){ SourceCity = new City(){ CityId = 1, Name = "Amsterdam" }, DestinationCity = new City(){ CityId = 4, Name = "Berlin"}, Distance = 900},
+                new Vertex(){ SourceCity = new City(){ CityId = 4, Name = "Berlin" }, DestinationCity = new City(){ CityId = 1, Name = "Amsterdam"}, Distance = 900},
+                new Vertex(){ SourceCity = new City(){ CityId = 4, Name = "Berlin" }, DestinationCity = new City(){ CityId = 2, Name = "Wroclaw"}, Distance = 350},
+                new Vertex(){ SourceCity = new City(){ CityId = 2, Name = "Wroclaw" }, DestinationCity = new City(){ CityId = 4, Name = "Berlin"}, Distance = 350}
             };
 
+            var listOfNeighboursDistance = new List<List<Vertex>>();
+
+            listOfNeighboursDistance.Add(new List<Vertex>());
+            Vertex zeroVertex = new Vertex() { SourceCity = new City(){ CityId = 0, Name = "0" }, DestinationCity = new City(){ CityId = 0, Name = "0"}, Distance = 0};
+            listOfNeighboursDistance[0].Add(zeroVertex);
+            
+            foreach(var p in preparedData)
+            {
+                listOfNeighboursDistance.Add(new List<Vertex>());
+                Vertex nextVertex = new Vertex() { SourceCity = new City(){ CityId = p.SourceCity.CityId, Name = p.SourceCity.Name }, DestinationCity = new City(){ CityId = p.DestinationCity.CityId, Name = p.DestinationCity.Name}, Distance = p.Distance};
+                listOfNeighboursDistance[p.SourceCity.CityId].Add(nextVertex);
+            }
+          
             var noCititesToSkip = Enumerable.Empty<string>();
 
             A.CallTo(() => repositoryMock.GetAll()).Returns(preparedData);
              
-            var result = service.FindShortestRoute(source, destination, noCititesToSkip);
+            var result = service.FindShortestRoute(source, destination, numberOfCities + 1, listOfNeighboursDistance);//noCititesToSkip);
 
             const int expectedDistance = 950;
 
